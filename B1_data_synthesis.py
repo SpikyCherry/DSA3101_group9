7b114db0313ab1640e02e0a2b6cdf33db7902530
TanXZfra <161848693+TanXZfra@users.noreply.github.com>
2025-03-12 22:12:03 +0800

    Update B1_data_synthesis.py
    Update interest table. Concatenate the two generated tables

--- B1_data_synthesis.py
+++ B1_data_synthesis.py
@@ -8,12 +8,12 @@ import os
 # Interest rates from 2008/7 to 2010/12, estimated from ECB
 # With the help of GenAI ChatGPT
 rate_table=[
-    {"time":"2008/7","MRO": 4.25,"current": 0.75,"three_months": 3.20,"six_months": 3.60,"one_year": 3.85,"two_year": 4.05},
-    {"time":"2008/12","MRO": 2.50,"current": 0.40,"three_months": 2.10,"six_months": 2.50,"one_year": 2.75,"two_year": 3.00},
-    {"time":"2009/5","MRO": 1.00,"current": 0.30,"three_months": 1.20,"six_months": 1.50,"one_year": 1.70,"two_year": 2.00},
-    {"time":"2009/12","MRO": 1.00,"current": 0.25,"three_months": 1.00,"six_months": 1.25,"one_year": 1.40,"two_year": 1.70},
-    {"time":"2010/6","MRO": 1.00,"current": 0.20,"three_months": 0.90,"six_months": 1.10,"one_year": 1.25,"two_year": 1.55},
-    {"time":"2010/12","MRO": 1.00,"current": 0.20,"three_months": 0.85,"six_months": 1.00,"one_year": 1.20,"two_year": 1.50}
+    {"time":"2008/7","current": 0.75,"three_months": 3.20,"six_months": 3.60,"one_year": 3.85,"two_year": 4.05},
+    {"time":"2008/12","current": 0.40,"three_months": 2.10,"six_months": 2.50,"one_year": 2.75,"two_year": 3.00},
+    {"time":"2009/5","current": 0.30,"three_months": 1.20,"six_months": 1.50,"one_year": 1.70,"two_year": 2.00},
+    {"time":"2009/12","current": 0.25,"three_months": 1.00,"six_months": 1.25,"one_year": 1.40,"two_year": 1.70},
+    {"time":"2010/6","current": 0.20,"three_months": 0.90,"six_months": 1.10,"one_year": 1.25,"two_year": 1.55},
+    {"time":"2010/12","current": 0.20,"three_months": 0.85,"six_months": 1.00,"one_year": 1.20,"two_year": 1.50}
 ]
 
 # Deposit terms
@@ -47,6 +47,8 @@ test=pd.read_csv(test_path,sep=";")
 train[["deposit_amount","term","interest_rate"]]=train["y"].apply(generate_deposit_features)
 test[["deposit_amount","term","interest_rate"]]=test["y"].apply(generate_deposit_features)
 
-train.to_csv(train_new_path,index=False)
-test.to_csv(test_new_path,index=False)
+test=test.iloc[1:]
+result=pd.concat([train,test],axis=0)
+
+result.to_csv(train_new_path,index=False)
 
